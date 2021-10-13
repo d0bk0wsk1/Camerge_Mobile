beabf749b477de3f567f59d9bd231c4771b5e2ed
paula dobkowski <paula.dobkowski@camerge.com.br>
2021-10-13 12:12:19 -0300

    Segunda Alteração 13/10

--- CamergeMobile/ClientApp/src/components/Home.js
+++ CamergeMobile/ClientApp/src/components/Home.js
@@ -4,7 +4,7 @@ import { WebView } from 'react';
 export class Home extends React.Component {
     static displayName = Home.name;
     render() {
-        window.location.replace('https://gestao.camerge.com.br/Admin/')
+        window.location.replace('https://gestao.camerge.com.br/Mobile/')
     }
 }
 
